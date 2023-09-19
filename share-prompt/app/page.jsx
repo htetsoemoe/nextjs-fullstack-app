c06b06ce069b01a4171465bcf7fca7935fe7532b
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-09-19 17:02:44 +0630

    finished tutorial project promptopia setup and running

---
+++ share-prompt/app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="mt-10 font-bold text-4xl">
+            Hello World from Home of 'Promptopia' using Next.js.
+        </div>
+    )
+}
+
+export default Home
